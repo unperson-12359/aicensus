992c2e415a2cfb10c320b58bedd72d8175c32bd3
mauriciogrs93 <mauriciogrs93@gmail.com>
2026-02-12 20:46:58 -0500

    Add Google Analytics tracking (G-0QBWVXJJQ0)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -89,6 +90,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} font-sans antialiased`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0QBWVXJJQ0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0QBWVXJJQ0');
+          `}
+        </Script>
         <ThemeProvider>
           <TooltipProvider>
             <div className="flex min-h-screen flex-col">{children}</div>
